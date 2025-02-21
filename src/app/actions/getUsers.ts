9620fe01ec1d58cd6397a5055b5edbc3cacaa6ad
Trung2204 <minhtrung9a8lhpqn@gmail.com>
2025-02-22 01:31:39 +0700

    feature: users, conversations page

---
+++ src/app/actions/getUsers.ts
@@ -0,0 +1,31 @@
+import prisma from "@/app/libs/prismadb";
+import getSession from "./getSession";
+
+const getUsers = async () => {
+  const session = await getSession();
+
+  if (!session?.user?.email) {
+    return [];
+  }
+
+  try {
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        NOT: {
+          email: session.user.email,
+        },
+      },
+    });
+
+    return users;
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  } catch (error: any) {
+    return [];
+  }
+};
+
+export default getUsers;

0b1fc7f98f1d8a7be10a2d3dd8ea127226301f0b
Trung2204 <minhtrung9a8lhpqn@gmail.com>
2025-02-24 10:24:29 +0700

    fix: log out stucked at /conversation/*

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/users/:path*"], // Protect the /users/* routes
+  matcher: ["/users/:path*", "/conversations/:path*"], // Protect the routes
 };
